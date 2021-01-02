7aecbb582a67653df412b02dfda3ca38d20c9147
Marcelo <santosmarcelo1995@live.com>
2021-01-02 13:11:03 -0300

    Method implementation

--- features/bootstrap/FormacaoEmMemoria.php
+++ features/bootstrap/FormacaoEmMemoria.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 
 use Alura\Armazenamento\Entity\Formacao;
@@ -7,16 +8,17 @@ use Alura\Armazenamento\Entity\Formacao;
 class FormacaoEmMemoria implements Context
 {    
     private string $mensagemDeErro = '';
+    private Formacao $formacao;
 
     /**
      * @When eu tentar criar uma formação com a descrição :arg1
      */
     public function euTentarCriarUmaFormacaoComADescricao(string $descricaoFormacao)
     {
-        $formacao = new Formacao();
+        $this->formacao = new Formacao();
 
         try {
-            $formacao->setDescricao($descricaoFormacao);
+            $this->formacao->setDescricao($descricaoFormacao);
         } catch (\InvalidArgumentException $exception) {
             $this->mensagemDeErro = $exception->getMessage(); 
         }
@@ -28,5 +30,13 @@ class FormacaoEmMemoria implements Context
     public function euVouVerASeguinteMensagemDeErro(string $mensagemDeErro)
     {
         assert($mensagemDeErro == $this->mensagemDeErro);
+    }
+
+    /**
+     * @Then eu devo ter uma formação criada com a descrição :arg1
+     */
+    public function euDevoTerUmaFormacaoCriadaComADescricao(string $descricaoFormacao)
+    {
+        assert($this->formacao->getDescricao() === $descricaoFormacao);
     }
 }
