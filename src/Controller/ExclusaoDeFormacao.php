f5815ae91d15cbc7582c090bf7a10b01810c070b
Marcelo <santosmarcelo1995@live.com>
2021-01-02 13:50:09 -0300

    Add ExclusaoDeFormacao controller

---
+++ src/Controller/ExclusaoDeFormacao.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Alura\Armazenamento\Controller;
+
+use Alura\Armazenamento\Entity\Formacao;
+use Alura\Armazenamento\Helper\MensagemFlash;
+use Doctrine\ORM\EntityManagerInterface;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ExclusaoDeFormacao implements RequestHandlerInterface
+{
+    use MensagemFlash;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $formacao = $this->entityManager->getReference(Formacao::class, $request->getQueryParams()['id']);
+        $this->entityManager->remove($formacao);
+        $this->entityManager->flush();
+        $this->adicionaMensagemFlash('success', 'Formação excluída com sucesso');
+
+        return new Response(302, ['Location' => '/listar-formacoes']);
+    }
+}
